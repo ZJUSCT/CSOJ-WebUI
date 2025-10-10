5c063867792b3339c75466d1c281ceb9ded7f3e4
eWloYW8 <3171132517@qq.com>
2025-10-10 14:43:29 +0800

    feat: remove API_BASE_URL

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL ? `${process.env.NEXT_PUBLIC_API_URL}/api/v1` : '/api/v1',
+  baseURL: '/api/v1',
 });
 
 api.interceptors.request.use(
